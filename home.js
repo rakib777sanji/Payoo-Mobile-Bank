02749d405197228115367cbe7d1b43385c11e3a9
rakib777sanji <litonpramanik3525@gmail.com>
2025-09-05 23:36:36 -1200

    worked in login form addeventlistener

--- home.js
+++ home.js
@@ -1,6 +1,18 @@
 document.getElementById("loginButton").
 addEventListener("click", function(e){
     e.preventDefault()
-    console.log("Login Clicked")
-    console.log(e)
+    const mobileNumber = 01988095193
+    const pinNumber = 9988
+    const mobileNumberValue = document.getElementById("mobile-number").value
+    const mobileNumberValueConverted = parseInt(mobileNumberValue)
+    const pinNumberValue = document.getElementById("pin-number").value
+    const pinNumberValueConverted = parseInt(pinNumberValue)
+    console.log(mobileNumberValueConverted, pinNumberValueConverted)
+
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+        console.log("All Values Matched")
+    }
+    else{
+        console.log("Invalid Credintials")
+    }
 })
