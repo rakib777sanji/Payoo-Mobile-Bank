be25056663a88db90810b733a28cbda330e66965
rakib777sanji <litonpramanik3525@gmail.com>
2025-09-06 23:44:58 -1200

    completed add money button half functionality

---
+++ home.js
@@ -0,0 +1,11 @@
+document.getElementById("add-money-btn").addEventListener("click", function(e){
+e.preventDefault()
+const bank = document.getElementById("bank").value
+const accountNumber = document.getElementById("account-number").value
+const amount = parseInt(document.getElementById("add-amount").value)
+const pin = document.getElementById("add-pin").value
+const availableBalance = parseInt(document.getElementById("available-balance").innerText)
+console.log(availableBalance)
+const totalNewAvailableBalance = amount + availableBalance
+document.getElementById("available-balance").innerText = totalNewAvailableBalance
+})
