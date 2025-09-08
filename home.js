248d350dd4c1c97c9904d8ef5003b5699a06bebf
rakib777sanji <litonpramanik3525@gmail.com>
2025-09-07 12:31:25 -1200

    completed add-money functionality

--- home.js
+++ home.js
@@ -1,11 +1,34 @@
+const validPin = 9988
+
 document.getElementById("add-money-btn").addEventListener("click", function(e){
 e.preventDefault()
 const bank = document.getElementById("bank").value
 const accountNumber = document.getElementById("account-number").value
 const amount = parseInt(document.getElementById("add-amount").value)
-const pin = document.getElementById("add-pin").value
+const pin = parseInt(document.getElementById("add-pin").value)
 const availableBalance = parseInt(document.getElementById("available-balance").innerText)
-console.log(availableBalance)
+console.log(amount, availableBalance)
+if (accountNumber.length <13) {
+    alert("Please provide a valid account number")
+    return;
+}
+if (pin !== validPin){
+    alert("Please provide a valid pin number")
+    return;
+}
 const totalNewAvailableBalance = amount + availableBalance
 document.getElementById("available-balance").innerText = totalNewAvailableBalance
+})
+
+
+// Toggling Feature */
+document.getElementById("add-mny-btn").addEventListener("click", function(){
+    document.getElementById("cash-out-parent").style.display = "none"
+    document.getElementById("add-money-parent").style.display = "block"
+})
+
+
+document.getElementById("cash-out-button").addEventListener("click", function(){
+    document.getElementById("add-money-parent").style.display = "none"
+    document.getElementById("cash-out-parent").style.display = "block"
 })
